2c7c32da6f1ce17fce5f0941bd081da3e22fa658
yeowon-yeong <yoy0123@naver.com>
2022-12-10 15:48:56 +0900

    add average

---
+++ Lv1/Average.java
@@ -0,0 +1,27 @@
+// 평균구하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12944
+package Lv1;
+
+import java.util.Arrays;
+
+public class Average {
+    public static void main(String[] args) {
+    int[] arr ={1,3,5,6,7,8,9,87,45,3};
+
+    double sum = 0;
+    for (int i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    double answer = sum / arr.length;
+    System.out.println("answer : " + answer);
+
+    // new solution
+    double new_answer = Arrays.stream(arr).average().getAsDouble();
+    System.out.println("new_answer : " + new_answer);
+
+    } // main
+} // class
+
+// 배워가는 사이트
+// average() : https://zetawiki.com/wiki/%EC%9E%90%EB%B0%94_average()
+// stream(람다) : https://coding-factory.tistory.com/574
