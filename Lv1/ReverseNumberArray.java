e8af64a4e1aa12015eafe72d28f27a066ef71887
yeowon-yeong <yoy0123@naver.com>
2022-12-13 10:04:39 +0900

    new cote

--- Lv1/ReverseNumberArray.java
+++ Lv1/ReverseNumberArray.java
@@ -1,23 +1,23 @@
-// ReverseNumberArray // 아직
+// ReverseNumberArray // Finish
 // https://school.programmers.co.kr/learn/courses/30/lessons/12932
 package Lv1;
 
 public class ReverseNumberArray {
     public static void main(String[] args) {
-        int[] answer = {};
-        int cnt = 0;
-        int n = 12345;
-        while (n>0) {
-            answer[cnt] = n % 10;
-            cnt += 1;
-            n /= 10;
-            System.out.println(cnt + ", " + n);
+        long n = 12345;
+        int len = (int)(Math.log10(n)+1); // 자릿수를 구하는 Math 활용
+        int[] answer = new int[len];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = Long.valueOf(n % 10).intValue(); // long2int 형변환
+            n/= 10;
         }
 
         for (int i = 0; i < answer.length; i++) {
-            System.out.println(answer[i]);
+            System.out.print(answer[i] + ", ");
         }
     } // main
 } // class
 
 // 배워가는 사이트
+// long을 int 형변환 : https://blog.naver.com/PostView.nhn?blogId=ka28&logNo=222104505635
+// 자릿수 구하기 : https://oneprogram.tistory.com/116
